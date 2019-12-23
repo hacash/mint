93aa8c075f881990edbb4c4930e7920454598c0f
jojoin <yangjie@jojoin.com>
2019-12-23 22:34:16 +0800

    miner pool auto transfer ok

---
+++ blockchain/rollback.go
@@ -0,0 +1,51 @@
+package blockchain
+
+import (
+	"fmt"
+	"github.com/hacash/core/blocks"
+	"github.com/hacash/core/interfaces"
+)
+
+func (bc *BlockChain) RollbackToBlockHeight(targetblockheight uint64) (uint64, error) {
+	lastest, err := bc.chainstate.ReadLastestBlockHeadAndMeta()
+	if err != nil {
+		return 0, err
+	}
+	last_hei := lastest.GetHeight()
+	if last_hei <= 1 || last_hei <= targetblockheight {
+		return last_hei, nil // end
+	}
+	fmt.Println("[BlockChain] Rollback to block height:", targetblockheight, "lastest height:", last_hei, "...")
+	var rollbackBlock interfaces.Block = nil
+	for i := lastest.GetHeight(); i >= 1; i-- {
+		// read block
+		_, blkdatas, e2 := bc.chainstate.BlockStore().ReadBlockBytesByHeight(i, 0)
+		if e2 != nil {
+			return 0, e2
+		}
+		block, _, e3 := blocks.ParseBlock(blkdatas, 0)
+		if e3 != nil {
+			return 0, e3
+		}
+		rollbackBlock = block
+		// check
+		if i == targetblockheight {
+			e5 := bc.chainstate.SetLastestBlockHeadAndMeta(rollbackBlock)
+			if e5 != nil {
+				return 0, e5
+			} else {
+				return i, nil // ok finish
+			}
+		} else {
+			// do rollback
+			e4 := block.RecoverChainState(bc.chainstate)
+			if e4 != nil {
+				return 0, e4
+			}
+			// do next
+		}
+	}
+
+	return 0, fmt.Errorf("error break.")
+
+}
