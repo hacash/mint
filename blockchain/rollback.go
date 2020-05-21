05a977ee2a7a4236531e1f6c553afa0657589232
jojoin <yangjie@jojoin.com>
2020-05-21 17:48:20 +0800

    add rollback height

--- blockchain/rollback.go
+++ blockchain/rollback.go
@@ -15,7 +15,7 @@ func (bc *BlockChain) RollbackToBlockHeight(targetblockheight uint64) (uint64, e
 	if last_hei <= 1 || last_hei <= targetblockheight {
 		return last_hei, nil // end
 	}
-	fmt.Println("[BlockChain] Rollback to block height:", targetblockheight, "lastest height:", last_hei, "...")
+	fmt.Print("[BlockChain] Rollback to block height:", targetblockheight, "lastest height:", last_hei, "... ")
 	var rollbackBlock interfaces.Block = nil
 	for i := lastest.GetHeight(); i >= 1; i-- {
 		// read block
@@ -34,6 +34,7 @@ func (bc *BlockChain) RollbackToBlockHeight(targetblockheight uint64) (uint64, e
 			if e5 != nil {
 				return 0, e5
 			} else {
+				fmt.Print("successfully !\n")
 				return i, nil // ok finish
 			}
 		} else {
