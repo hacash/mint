9dad37102e16752b7c2c316ba81ae42807c749d0
jojoin <yangjie@jojoin.com>
2021-08-28 21:12:36 +0800

    + channel pay routes publish

--- consts.go
+++ consts.go
@@ -1,12 +1,12 @@
 package mint
 
 var (
-	SingleBlockMaxSize uint32 = 1024 * 1024 * 1 // 1MB
+	SingleBlockMaxSize uint32 = 1000 * 1000 * 1 // ≈1MB
 
 	AdjustTargetDifficultyNumberOfBlocks uint64 = 288 // about one day
 	EachBlockRequiredTargetTime          uint64 = 300 // 60 * 5 , about 5 min
 
-	MinTransactionFeePurityOfOneByte     uint64 = 10000 * 10000 * 10000 / 200 // = 5000000000:232 = 50铢 max 1844 67440737 09551615
+	MinTransactionFeePurityOfOneByte uint64 = 10000 * 10000 * 10000 / 200 // = 5000000000:232 = 50铢 max 1844 67440737 09551615
 	// one simple trs bytes length = 2+21+10+5+21+10+2+33+64+2 = 170 ~ 200
 
 )
